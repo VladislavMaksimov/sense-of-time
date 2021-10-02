8b47191d50b3491d8f3237f99cc19b38caa9d486
VladislavMaksimov <workmaximov@yandex.ru>
2021-10-02 23:27:49 +0500

    add background

--- index.js
+++ index.js
@@ -1,30 +1,115 @@
+const backgroundWidth = 500;
+const backgroundHeight = 300;
+
+// создаёт ось
+const createCoordLine = (x, y, endX, endY, pointerLength) => {
+  const pointerWidth = 10;
+  const color = "#000000";
+  const strokeWidth = 4;
+  const lineJoin = "miter";
+  const lineCap = "butt";
+
+  return new Konva.Arrow({
+    x: x,
+    y: y,
+    points: [0, 0, endX, endY],
+    pointerLength: pointerLength,
+    pointerWidth: pointerWidth,
+    fill: color,
+    stroke: color,
+    strokeWidth: strokeWidth,
+    lineJoin: lineJoin,
+    lineCap: lineCap,
+  });
+};
+
+// создаёт оси X и Y
+const addCoordLines = (background) => {
+  const pointerLength = 10;
+  const coordLineX = createCoordLine(
+    0,
+    backgroundHeight / 2,
+    backgroundWidth - pointerLength / 2,
+    0,
+    pointerLength
+  );
+  const coordLineY = createCoordLine(
+    backgroundWidth / 2,
+    backgroundHeight,
+    0,
+    -1 * (backgroundHeight - pointerLength / 2),
+    pointerLength
+  );
+
+  background.add(coordLineX);
+  background.add(coordLineY);
+};
+
+// создаёт текст на оси Y
+const addCoordText = (background) => {
+  // общие значения
+  const fontSize = 16;
+  const textColor = "#000000";
+  const leftTextContent = "Прошлое";
+  const rightTextContent = "Настоящее";
+  const fontFamily = "Calibri";
+
+  // координаты начала текстов
+  const leftX = 0;
+  const leftY = backgroundHeight / 2 - fontSize * 2;
+  const rightX = backgroundWidth - rightTextContent.length * (fontSize / 1.8);
+  const rightY = backgroundHeight / 2 - fontSize * 2;
+
+  const leftText = new Konva.Text({
+    x: leftX,
+    y: leftY,
+    text: leftTextContent,
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    fill: textColor,
+  });
+
+  const rightText = new Konva.Text({
+    x: rightX,
+    y: rightY,
+    text: rightTextContent,
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    fill: textColor,
+  });
+
+  background.add(leftText);
+  background.add(rightText);
+};
+
+const addBackgroundColor = (background, color) => {
+  var backgroundColor = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: backgroundWidth,
+    height: backgroundHeight,
+    fill: color,
+  });
+  background.add(backgroundColor);
+};
+
+const drawBackground = (stage) => {
+  const background = new Konva.Layer();
+
+  addBackgroundColor(background, "#c2c0ba30");
+  addCoordLines(background, stage);
+  addCoordText(background, stage);
+
+  stage.add(background);
+  background.draw();
+};
+
 window.addEventListener("load", () => {
-  // first we need to create a stage
-  var stage = new Konva.Stage({
-    container: "container", // id of container <div>
-    width: 300,
-    height: 300,
+  const stage = new Konva.Stage({
+    container: "questionnaire",
+    width: backgroundWidth,
+    height: backgroundHeight,
   });
 
-  // then create layer
-  var layer = new Konva.Layer();
-
-  // create our shape
-  var circle = new Konva.Circle({
-    x: stage.width() / 2,
-    y: stage.height() / 2,
-    radius: 70,
-    fill: "red",
-    stroke: "black",
-    strokeWidth: 4,
-  });
-
-  // add the shape to the layer
-  layer.add(circle);
-
-  // add the layer to the stage
-  stage.add(layer);
-
-  // draw the image
-  layer.draw();
+  drawBackground(stage);
 });
